75e68804c57f3fb170b66051ab0ed586cde8d803
Pear1y <gh_wns@naver.com>
2024-08-20 23:32:56 +0900

    Add files via upload

---
+++ test/JavaScript(Core)/testQuestion3.js
@@ -0,0 +1,15 @@
+// 프로퍼티 추가 & 제거
+
+var fruit = {
+    name : 'apple',
+    color : 'green'
+
+};
+
+// 마침표 표기법으로 색깔을 red로 변경해보시오.
+
+
+// 대괄호 표기법으로 새로운 프로퍼티를 추가해보시오. (key : shape, value : circle)
+
+
+//
