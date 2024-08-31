33bd3cd07f20807092ba0831802b041bd13a4fa9
Pear1y <gh_wns@naver.com>
2024-08-31 12:31:08 +0900

    Add files via upload

---
+++ test/testQuestion1.js
@@ -0,0 +1,7 @@
+<div id="headerid" class="maincls"></div>
+// headerid에 대한 노드를 $header라는 변수로 구하고
+// maincls에 대해 CSS 선택자를 이용해 $main이라는 변수로 노드 취득을 해보시오.
+
+const $header = document.getElementById('header'); // id 취득
+
+const $main = document.querySelector('.maincls'); // class(css 선택자) 취득
